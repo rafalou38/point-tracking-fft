2f623f13b0a689beb51b07f2008bf05215545b28
Rafael <rafael.martinezcalvo@orange.fr>
2023-11-19 21:49:23 +0100

    maj script

--- view.py
+++ view.py
@@ -6,12 +6,7 @@ from scipy.optimize import curve_fit
 
 
 def expAmortie(t, a,tau,f0,phi,b):
-    # a = 60
-    # b = 4
-    # f0 = 3.45
-    # d = 1.9
-    # e = 295
-    return a * np.exp(-t / b) * np.cos(2 * np.pi * t * f0 + phi) + b
+    return a * np.exp(-t / tau) * np.cos(2 * np.pi * t * f0 + phi) + b
 
 
 # Définis la fonction lorentzienne
@@ -20,17 +15,38 @@ def lorentz(x, amplitude, center, width):
     # return (2/np.pi*width) / (1+((x-center)/(width/2))**2)
 
 
-SKIP = 10
 
 def viewData(file):
     # Data: t, x,y
-    df = pd.read_csv(file+".csv")
+    df = pd.read_csv(file+".csv", nrows=600)
 
     plt.figure(figsize=(15, 8))
 
     plt.subplot(2, 1, 1)
     plt.scatter(df["t"], df["y"], linewidth=1, s=4, marker="x")
-    plt.plot(df["t"], df["y"], linewidth=1)
+
+    try:
+        print("Modélisation exponentielle amortie")
+        expOpts, _ = curve_fit(expAmortie, np.abs(df["t"]), np.abs(df["y"]), [
+            -16.5,  # a
+            2.3,    # tau
+            7.8,   # f0
+            -0.1,  # phi
+            97,     # b
+        ])
+        a,tau,f0,phi,b = expOpts
+        # plt.annotate(
+        #     f"f(x)=a/π*(w/2)/((x-c)^2+(w/x0)^2)\n\na = {amplitude:.2f}\nx0 = {center:.2f}\nw = {width:.2f}",
+        #     xy=(0.75, 0.25),
+        #     xycoords="subfigure fraction",
+        #     fontsize=11,
+        # )
+        # aMax = np.max(np.abs(yf))
+
+        # print(amplitude, center, width, aMax)
+        plt.plot(df["t"], expAmortie(df["t"], *expOpts), "r-", linewidth=1)
+    except:
+        pass
 
     plt.title("Y en fonction de t")
 
@@ -38,19 +54,25 @@ def viewData(file):
     plt.ylabel("y (px)")
 
     # FT
+    FT_LO = 10
+    FT_HI = 50
+
     plt.subplot(2, 1, 2)
     yf = scipy.fftpack.fft(df["y"])
-    xf = np.fft.fftfreq(len(yf), df["t"][1] - df["t"][0])[SKIP:-SKIP]
-    yf = yf[SKIP:-SKIP]
+    xf = np.fft.fftfreq(len(yf), df["t"][1] - df["t"][0])[FT_LO:FT_HI]
+    yf = yf[FT_LO:FT_HI]
 
     plt.scatter(np.abs(xf), np.abs(yf), linewidth=1, s=6, marker="x")
-    plt.title("FFT de Y")
+    plt.title("Transformée de Fourier de y")
     plt.xlabel("Fréquence (Hz)")
     plt.ylabel("Amplitude")
 
     # Modélisation
     try:
+        xm = np.linspace(3, 16, 1000)
+        # plt.plot(xm, lorentz(xm, *expOpts), "b-", linewidth=1)
         lorentzOpts, pcov = curve_fit(lorentz, np.abs(xf), np.abs(yf))
+        ym=lorentz(np.abs(xm), *lorentzOpts)
         amplitude, center, width = lorentzOpts
         plt.annotate(
             f"f(x)=a/π*(w/2)/((x-c)^2+(w/x0)^2)\n\na = {amplitude:.2f}\nx0 = {center:.2f}\nw = {width:.2f}",
@@ -58,10 +80,10 @@ def viewData(file):
             xycoords="subfigure fraction",
             fontsize=11,
         )
-        aMax = np.max(np.abs(yf))
+        aMax = np.max(np.abs(ym))
 
         print(amplitude, center, width, aMax)
-        plt.plot(np.abs(xf), lorentz(np.abs(xf), *lorentzOpts), "r-", linewidth=1)
+        plt.plot(np.abs(xm), ym, "r-", linewidth=1)
 
         # Display in graph (line)
         x1 = center - (width / 2) * np.sqrt((np.sqrt(2) - 1))
@@ -86,4 +108,4 @@ def viewData(file):
 
 
 if __name__ == "__main__":
-    viewData("18-nov/18nov-libre plastique-3g0-2aimants.avi")
+    viewData("18-nov/18nov-libre plastique-0g0-0aimants.avi")
